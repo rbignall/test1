1cb21291a629b7d81758f5fe28f4c989d7d12402
Rupert Bignall <rupert.bignall@capita.co.uk>
2019-01-14 14:55:59 +0000

    Still mysql problems.

--- kafka-cluster-install.sh
+++ kafka-cluster-install.sh
@@ -260,7 +260,7 @@ install_mysql()
   log "Installing MySql"
 
   apt-get -y update
-  DEBIAN_FRONTEND=noninteractive apt-get -yq install mysql-server php5-mysql
+  DEBIAN_FRONTEND=noninteractive apt-get -q -y -o Dpkg::Options::="--force-confnew" install mysql-server php5-mysql
 
   mysql_install_db
 
