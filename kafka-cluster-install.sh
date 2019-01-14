5f72328de6eee57cd848b5e2e9358292d3b20509
Rupert Bignall <rupert.bignall@capita.co.uk>
2019-01-14 12:17:37 +0000

    Set root password on mysql not working

--- kafka-cluster-install.sh
+++ kafka-cluster-install.sh
@@ -254,7 +254,7 @@ install_mysql()
 
   mysql_install_db
   # actions from mysql_secure_installation (roughly)
-  mysql -e "UPDATE mysql.user SET Password = PASSWORD('${MYSQLPASSWORD}') WHERE User = 'root'"
+  mysql -e "UPDATE mysql.user SET Password = PASSWORD('"${MYSQLPASSWORD}"') WHERE User = 'root'"
   mysql -e "DELETE FROM mysql.user WHERE User=''"
   mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
   mysql -e "DROP DATABASE IF EXISTS test"
